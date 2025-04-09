6c9dfdfcad2e1e2841acb96ce5e5a00097a8ce3b
zenhog <61523958+zenhog@users.noreply.github.com>
2025-04-09 01:20:32 +0100

    2025-04-09 at 01:20:32 on epicenter

--- config/awesome/commands.lua
+++ config/awesome/commands.lua
@@ -1,126 +1,137 @@
-local awful = require('awful')
-local wibox = require('wibox')
+local awful = require("awful")
+local wibox = require("wibox")
 --dbus = nil
-local naughty = require('naughty')
-local theme = require('beautiful')
-local sharedtags = require('sharedtags')
+local naughty = require("naughty")
+local theme = require("beautiful")
+local sharedtags = require("sharedtags")
 
 local commands = {}
 
 commands.focus_up = function()
-    awful.client.focus.bydirection("up")
+	awful.client.focus.bydirection("up")
 end
 
 commands.focus_down = function()
-    awful.client.focus.bydirection("down")
+	awful.client.focus.bydirection("down")
 end
 
 commands.focus_left = function()
-    awful.client.focus.bydirection("left")
+	awful.client.focus.bydirection("left")
 end
 
 commands.focus_right = function()
-    awful.client.focus.bydirection("right")
+	awful.client.focus.bydirection("right")
 end
 
-commands.next_client = function()
-    awful.client.focus.byidx(1)
+commands.swap_up = function()
+	awful.client.swap.bydirection("up")
 end
 
-commands.prev_client = function()
-    awful.client.focus.byidx(-1)
+commands.swap_down = function()
+	awful.client.swap.bydirection("down")
 end
 
-commands.unminimize = function()
-    local c = awful.client.restore()
-    if c then
-        c:emit_signal("request::activate", "key.unminimize", { raise = true })
-    end
+commands.swap_left = function()
+	awful.client.swap.bydirection("left")
 end
 
-commands.swap_up = nil
-commands.swap_down = nil
-commands.swap_left = nil
-commands.swap_right = nil
+commands.swap_right = function()
+	awful.client.swap.bydirection("right")
+end
 
 commands.resize_up = nil
 commands.resize_down = nil
 commands.resize_left = nil
 commands.resize_right = nil
 
-commands.layout = function()
-  awful.layout.inc(1)
+commands.next_client = function()
+	awful.client.focus.byidx(1)
 end
 
+commands.prev_client = function()
+	awful.client.focus.byidx(-1)
+end
+
+commands.unminimize = function()
+	local c = awful.client.restore()
+	if c then
+		c:emit_signal("request::activate", "key.unminimize", { raise = true })
+	end
+end
+
+commands.layout = function()
+	awful.layout.inc(1)
+end
 
 commands.gototag = function(tagnum)
-  return function()
-    --local screen = awful.screen.focused()
-    --local tag = screen.tags[tagnum]
-    --if tag then tag:view_only() end
-    local tag = _G.tags[tagnum]
+	return function()
+		--local screen = awful.screen.focused()
+		--local tag = screen.tags[tagnum]
+		--if tag then tag:view_only() end
+		local tag = _G.tags[tagnum]
 
-    if tag then
-      local screen = _G.tags[tagnum].screen
-      awful.screen.focus(screen)
-      sharedtags.viewonly(tag, screen)
-    end
-
-  end
+		if tag then
+			local screen = _G.tags[tagnum].screen
+			awful.screen.focus(screen)
+			sharedtags.viewonly(tag, screen)
+		end
+	end
 end
 
 commands.movetotag = function(tagnum)
-  return function()
-    if client.focus then
-      --local tag = client.focus.screen.tags[tagnum]
-      local tag = _G.tags[tagnum]
-      --if tag then client.focus:move_to_tag(tag) end
-      if tag then client.focus:move_to_tag(tag) end
-    end
-  end
+	return function()
+		if client.focus then
+			--local tag = client.focus.screen.tags[tagnum]
+			local tag = _G.tags[tagnum]
+			--if tag then client.focus:move_to_tag(tag) end
+			if tag then
+				client.focus:move_to_tag(tag)
+			end
+		end
+	end
 end
 
 commands.toggletag = function(tagnum)
-  return function()
-    --local screen = awful.screen.focused()
-    --local tag = screen.tags[tagnum]
-    local tag = _G.tags[tagnum]
-    --if tag then awful.tag.viewtoggle(tag) end
-    --if tag then sharedtags.viewtoggle(tag, screen) end
-    if tag then
-      local screen = _G.tags[tagnum].screen
-      --awful.screen.focus(screen)
-      sharedtags.viewtoggle(tag, screen)
-    end
-  end
+	return function()
+		--local screen = awful.screen.focused()
+		--local tag = screen.tags[tagnum]
+		local tag = _G.tags[tagnum]
+		--if tag then awful.tag.viewtoggle(tag) end
+		--if tag then sharedtags.viewtoggle(tag, screen) end
+		if tag then
+			local screen = _G.tags[tagnum].screen
+			--awful.screen.focus(screen)
+			sharedtags.viewtoggle(tag, screen)
+		end
+	end
 end
 
 commands.kill_alerts = function()
-    naughty.destroy_all_notifications()
+	naughty.destroy_all_notifications()
 end
 
 commands.vtswitch = function(i)
-    return function()
-        awful.spawn.with_shell('sudo chvt ' .. tostring(i))
-    end
+	return function()
+		awful.spawn.with_shell("sudo chvt " .. tostring(i))
+	end
 end
 
 commands.fullscreen = function(c)
-    c.fullscreen = not c.fullscreen
-    c:raise()
+	c.fullscreen = not c.fullscreen
+	c:raise()
 end
 
 commands.minimize = function(c)
-    c.minimized = true
+	c.minimized = true
 end
 
 commands.maximize = function(c)
-    c.maximized = not c.maximized
-    c:raise()
+	c.maximized = not c.maximized
+	c:raise()
 end
 
 commands.close = function(c)
-    c:kill()
+	c:kill()
 end
 
 return commands
--- config/awesome/keys.lua
+++ config/awesome/keys.lua
@@ -1,7 +1,7 @@
-local awful = require('awful')
-local gears = require('gears')
-local commands = require('commands')
-local hotkeys_popup = require('awful.hotkeys_popup')
+local awful = require("awful")
+local gears = require("gears")
+local commands = require("commands")
+local hotkeys_popup = require("awful.hotkeys_popup")
 
 local altkey = "Mod1"
 local modkey = "Mod4"
@@ -9,121 +9,123 @@ local modkey = "Mod4"
 local mods = {}
 
 local nmod = { modkey }
-local smod = { modkey, "Shift"   }
+local smod = { modkey, "Shift" }
 local cmod = { modkey, "Control" }
 
-mods['E'] = {}
-mods['W'] = nmod
-mods['S-W'] = smod
-mods['C-W'] = cmod
-mods['S-M'] = { altkey, "Shift" }
-mods['C-M'] = { altkey, "Control" }
-
+mods["E"] = {}
+mods["W"] = nmod
+mods["S-W"] = smod
+mods["C-W"] = cmod
+mods["S-M"] = { altkey, "Shift" }
+mods["C-M"] = { altkey, "Control" }
 
 local mapkey = function(mod, key, cmd, group, desc)
-    return awful.key(mod, key, cmd, {
-        group = group,
-        description = desc,
-    })
+	return awful.key(mod, key, cmd, {
+		group = group,
+		description = desc,
+	})
 end
 
 local spawn = function(cmd)
-    return function()
-        awful.spawn(cmd)
-    end
+	return function()
+		awful.spawn(cmd)
+	end
 end
 
-
 local pipexec = function(cmd)
-    local pipe = io.popen(cmd)
-    local u = {}
-    for line in pipe:lines() do
-        table.insert(u, line)
-    end
-    pipe:close()
-    return u
+	local pipe = io.popen(cmd)
+	local u = {}
+	for line in pipe:lines() do
+		table.insert(u, line)
+	end
+	pipe:close()
+	return u
 end
 
 globalkeys = {}
 clientkeys = {}
 
 clientkeys = gears.table.join(
-    mapkey(nmod, "f", commands.fullscreen,  "client", "fullscreen"),
-    mapkey(nmod, "x", commands.maximize,    "client", "maximize"),
-    mapkey(nmod, "n", commands.minimize,    "client", "minimize"),
-    mapkey(nmod, "q", commands.close,       "client", "close"),
-    -- o: make-master-window
-    {}
+	mapkey(nmod, "f", commands.fullscreen, "client", "fullscreen"),
+	mapkey(nmod, "x", commands.maximize, "client", "maximize"),
+	mapkey(nmod, "n", commands.minimize, "client", "minimize"),
+	mapkey(nmod, "q", commands.close, "client", "close"),
+	-- o: make-master-window
+	{}
 )
 
 globalkeys = gears.table.join(
-    mapkey(nmod, ",", awful.tag.viewprev, "tag", "view prev"),
-    mapkey(nmod, ".", awful.tag.viewnext, "tag", "view next"),
-    mapkey(smod, "s", hotkeys_popup.show_help, "awesome", "help popup"),
+	mapkey(nmod, ",", awful.tag.viewprev, "tag", "view prev"),
+	mapkey(nmod, ".", awful.tag.viewnext, "tag", "view next"),
+	mapkey(smod, "s", hotkeys_popup.show_help, "awesome", "help popup"),
 
-    mapkey(nmod, "k", commands.focus_up,    "client", "focus up"),
-    mapkey(nmod, "j", commands.focus_down,  "client", "focus down"),
-    mapkey(nmod, "h", commands.focus_left,  "client", "focus left"),
-    mapkey(nmod, "l", commands.focus_right, "client", "focus right"),
-    -- C-hjkl: swap-by-direction?
-    -- HJKL: resize-by-direction?
+	mapkey(nmod, "k", commands.focus_up, "client", "focus up"),
+	mapkey(nmod, "j", commands.focus_down, "client", "focus down"),
+	mapkey(nmod, "h", commands.focus_left, "client", "focus left"),
+	mapkey(nmod, "l", commands.focus_right, "client", "focus right"),
+	mapkey(smod, "k", commands.swap_up, "client", "swap up"),
+	mapkey(smod, "j", commands.swap_down, "client", "swap down"),
+	mapkey(smod, "h", commands.swap_left, "client", "swap left"),
+	mapkey(smod, "l", commands.swap_right, "client", "swap right"),
+	-- C-hjkl: swap-by-direction?
+	-- HJKL: resize-by-direction?
 
-    mapkey(smod, "w", commands.kill_alerts, "awesome", "kill alerts"),
-    mapkey(nmod, "m", commands.unminimize,  "awesome", "unminimize"),
+	mapkey(smod, "w", commands.kill_alerts, "awesome", "kill alerts"),
+	mapkey(nmod, "m", commands.unminimize, "awesome", "unminimize"),
 
-    mapkey(nmod, "Tab", commands.next_client,  "awesome", "next client"),
-    mapkey(smod, "Tab", commands.prev_client,  "awesome", "prev client"),
+	mapkey(nmod, "Tab", commands.next_client, "awesome", "next client"),
+	mapkey(smod, "Tab", commands.prev_client, "awesome", "prev client"),
 
-    mapkey(nmod, "space", commands.layout,        "awesome", "toggle layout"),
-    mapkey(nmod, "u", awful.client.urgent.jumpto, "awesome", "jump urgent"),
+	mapkey(nmod, "space", commands.layout, "awesome", "toggle layout"),
+	mapkey(nmod, "u", awful.client.urgent.jumpto, "awesome", "jump urgent"),
 
-    mapkey(cmod, "q", awesome.quit,    "awesome", "quit awesome"),
-    mapkey(cmod, "r", awesome.restart, "awesome", "restart awesome"),
-    {}
+	mapkey(cmod, "q", awesome.quit, "awesome", "quit awesome"),
+	mapkey(cmod, "r", awesome.restart, "awesome", "restart awesome"),
+	{}
 )
 
 for i = 1, 9 do
-    globalkeys = gears.table.join(globalkeys,
-        mapkey(nmod, "#" .. i + 9, commands.gototag(i)),
-        mapkey(smod, "#" .. i + 9, commands.movetotag(i)),
-        mapkey(cmod, "#" .. i + 9, commands.toggletag(i)),
-        mapkey({ altkey }, "F" .. i,     commands.vtswitch(i)),
-        {}
-    )
+	globalkeys = gears.table.join(
+		globalkeys,
+		mapkey(nmod, "#" .. i + 9, commands.gototag(i)),
+		mapkey(smod, "#" .. i + 9, commands.movetotag(i)),
+		mapkey(cmod, "#" .. i + 9, commands.toggletag(i)),
+		mapkey({ altkey }, "F" .. i, commands.vtswitch(i)),
+		{}
+	)
 end
 
-for _, line in ipairs(pipexec('gui')) do
-    local _, command, mod, key = line:match('^(%S+):(%S+):(%S+):(%S+)$')
+for _, line in ipairs(pipexec("gui")) do
+	local _, command, mod, key = line:match("^(%S+):(%S+):(%S+):(%S+)$")
 
-    globalkeys = gears.table.join(globalkeys,
-        mapkey(mods[mod], key,
-            spawn(string.format('menu loop %s', command)), "menu", command)
-    )
+	globalkeys = gears.table.join(
+		globalkeys,
+		mapkey(mods[mod], key, spawn(string.format("menu loop %s", command)), "menu", command)
+	)
 end
 
-for _, line in ipairs(pipexec('gui run keys')) do
-    local app, mod, key = line:match('^(%S+):(%S+):(%S+)$')
+for _, line in ipairs(pipexec("gui run keys")) do
+	local app, mod, key = line:match("^(%S+):(%S+):(%S+)$")
 
-    globalkeys = gears.table.join(globalkeys,
-        mapkey(mods[mod], key,
-            spawn(string.format('gui run fg %s', app)), "app", app)
-    )
+	globalkeys =
+		gears.table.join(globalkeys, mapkey(mods[mod], key, spawn(string.format("gui run fg %s", app)), "app", app))
 end
 
-for _, line in ipairs(pipexec('widget')) do
-    local widget, _ = line:match('^(%S+):(%S+)$')
-    for _, l in ipairs(pipexec(string.format('widget %s keys', widget))) do
-        local method, mod, key = l:match('^(%S+):(%S+):(%S+)$')
-        globalkeys = gears.table.join(globalkeys,
-            mapkey(
-                mods[mod],
-                key,
-                spawn(string.format('widget %s %s', widget, method)),
-                "widget",
-                string.format('%s %s', widget, method)
-            )
-        )
-    end
+for _, line in ipairs(pipexec("widget")) do
+	local widget, _ = line:match("^(%S+):(%S+)$")
+	for _, l in ipairs(pipexec(string.format("widget %s keys", widget))) do
+		local method, mod, key = l:match("^(%S+):(%S+):(%S+)$")
+		globalkeys = gears.table.join(
+			globalkeys,
+			mapkey(
+				mods[mod],
+				key,
+				spawn(string.format("widget %s %s", widget, method)),
+				"widget",
+				string.format("%s %s", widget, method)
+			)
+		)
+	end
 end
 
 root.keys(globalkeys)
