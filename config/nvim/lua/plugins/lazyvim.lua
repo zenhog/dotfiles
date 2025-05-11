bf91c6311d598f033a6973a30038ff39cade4dd9
zenhog <61523958+zenhog@users.noreply.github.com>
2025-05-11 17:00:55 +0100

    2025-05-11 at 17:00:55 on epicenter

---
+++ config/nvim/lua/plugins/lazyvim.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+      defaults = {
+        keymaps = false,
+      },
+    },
+  },
+}
---
+++ config/nvim/lua/plugins/onedarkpro.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      require("onedarkpro").setup({
+        vim.cmd("colorscheme onedark"),
+      })
+    end,
+  },
+}
