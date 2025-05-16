19188b7e46c1d8057646f01c83d0a3d1ce8ab8fc
zenhog <61523958+zenhog@users.noreply.github.com>
2025-05-16 13:03:08 +0100

    2025-05-16 at 13:03:07 on stucksland

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,8 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
-vim.g.mapleader = "\\"
+
+vim.g.theme_dark = "catppuccin-mocha"
+vim.g.theme_dark = "tokyonight-night"
+vim.g.theme_light = "gruvbox"
+
+local theme = vim.fn.systemlist("cat $HOME/.colorscheme")[1]
+
+if theme == "" or not theme then
+  theme = "dark"
+end
+
+-- this needs to be put in a utils.lua and be required from plugin config
+-- vim.g.theme_current = theme or "dark"
+-- vim.cmd(string.format("colorscheme %s", vim.g["theme_" .. vim.g.theme_current]))
+-- vim.opt.bg = theme
+
+vim.g.mapleader = ""
 vim.g.maplocalleader = " "
 
 vim.g.mapleader = false
