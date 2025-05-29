997d8c67a401c6b701755fc7cab2b396f9c4682a
zenhog <61523958+zenhog@users.noreply.github.com>
2025-05-29 02:27:52 +0100

    2025-05-29 at 02:27:52 on stucksland

--- config/nvim/lua/plugins/whichkey.lua
+++ config/nvim/lua/plugins/whichkey.lua
@@ -1,58 +1,60 @@
 return {
   {
     "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts_extend = { "spec" },
-    opts = {
-      preset = "helix",
-      defaults = {},
-      spec = {
-        {
-          mode = { "n", "v" },
-          { "[", group = "prev" },
-          { "]", group = "next" },
-          { "g", group = "goto" },
-          { "gs", group = "surround" },
-          { "z", group = "fold" },
+    -- event = "VeryLazy",
+    -- opts_extend = { "spec" },
+    config = function()
+      require("which-key").setup({
+        preset = "classic",
+        -- defaults = {},
+        spec = {
           {
-            "<leader>b",
-            group = "buffer",
-            expand = function()
-              return require("which-key.extras").expand.buf()
-            end,
+            mode = { "n", "v" },
+            { "[", group = "prev" },
+            { "]", group = "next" },
+            { "g", group = "goto" },
+            { "gs", group = "surround" },
+            { "z", group = "fold" },
+            {
+              "<leader>b",
+              group = "buffer",
+              expand = function()
+                return require("which-key.extras").expand.buf()
+              end,
+            },
+            {
+              "<leader>w",
+              group = "windows",
+              proxy = "<c-w>",
+              expand = function()
+                return require("which-key.extras").expand.win()
+              end,
+            },
+            -- better descriptions
+            { "gx", desc = "Open with system app" },
           },
-          {
-            "<leader>w",
-            group = "windows",
-            proxy = "<c-w>",
-            expand = function()
-              return require("which-key.extras").expand.win()
-            end,
+        },
+        plugins = {
+          marks = true, -- shows a list of your marks on ' and `
+          registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+          -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+          -- No actual key bindings are created
+          spelling = {
+            enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+            suggestions = 20, -- how many suggestions should be shown in the list?
           },
-          -- better descriptions
-          { "gx", desc = "Open with system app" },
+          presets = {
+            operators = true, -- adds help for operators like d, y, ...
+            motions = true, -- adds help for motions
+            text_objects = true, -- help for text objects triggered after entering an operator
+            windows = true, -- default bindings on <c-w>
+            nav = true, -- misc bindings to work with windows
+            z = true, -- bindings for folds, spelling and others prefixed with z
+            g = true, -- bindings for prefixed with g
+          },
         },
-      },
-      plugins = {
-        marks = true, -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-        -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-        -- No actual key bindings are created
-        spelling = {
-          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-          suggestions = 20, -- how many suggestions should be shown in the list?
-        },
-        presets = {
-          operators = true, -- adds help for operators like d, y, ...
-          motions = true, -- adds help for motions
-          text_objects = true, -- help for text objects triggered after entering an operator
-          windows = true, -- default bindings on <c-w>
-          nav = true, -- misc bindings to work with windows
-          z = true, -- bindings for folds, spelling and others prefixed with z
-          g = true, -- bindings for prefixed with g
-        },
-      },
-    },
+      })
+    end,
     keys = {
       {
         "<leader>?",
@@ -69,13 +71,13 @@ return {
         desc = "Window Hydra Mode (which-key)",
       },
     },
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.setup(opts)
-      if not vim.tbl_isempty(opts.defaults) then
-        LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
-        wk.register(opts.defaults)
-      end
-    end,
+    -- config = function(_, opts)
+    --   local wk = require("which-key")
+    --   wk.setup(opts)
+    --   if not vim.tbl_isempty(opts.defaults) then
+    --     LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
+    --     wk.register(opts.defaults)
+    --   end
+    -- end,
   },
 }
