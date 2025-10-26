d44ad31df8a0b0c71a28bdc3d2d510f89be5083b
zenhog <61523958+zenhog@users.noreply.github.com>
2025-10-26 03:44:44 +0100

    2025-10-26 at 03:44:44 on epicenter

--- config/tridactyl/surfingkeys.js
+++ config/tridactyl/surfingkeys.js
@@ -91,14 +91,14 @@ mapkey("gx", 'List all role=button in spotify', function() {
   });
 
   Front.openOmnibar({
-    // type: "Custom",
-    // title: "Spotify playlists",
-    type: "UserURLs",
+    type: "Custom",
+    title: "Spotify playlists",
+    // type: "UserURLs",
     extra: items,
-    // extra: {
-    //   title: 'Spotify',
-    //   items: items,
-    // },
+    extra: {
+      title: 'Spotify',
+      items: items,
+    },
   })
 })
 
