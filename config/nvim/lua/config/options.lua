5115f8c35ba3fa886ede978371e12267111c4e5b
zenhog <61523958+zenhog@users.noreply.github.com>
2026-07-15 04:42:17 +0100

    2026-07-15 at 04:42:17 on epicenter

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+---- This file is automatically loaded by plugins.core
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = " "
+
+-- LazyVim auto format
+vim.g.autoformat = false
