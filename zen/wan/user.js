e515b12fd099f081075e1474639b54929c1231df
zenhog <61523958+zenhog@users.noreply.github.com>
2026-07-17 03:58:04 +0100

    2026-07-17 at 03:58:04 on epicenter

---
+++ zen/wan/user.js
@@ -0,0 +1,28 @@
+//devtools.everOpened
+//browser.urlbar.shortcuts.history
+//browser.urlbar.shortcuts.showSearchSuggestionsFirst
+//browser.urlbar.shortcuts.bookmarks
+//browser.urlbar.shortcuts.tabs
+//browser.urlbar.suggest.amp
+//browser.urlbar.suggest.speculativeConnect.enabled
+//browser.urlbar.suggest.clipboard
+//browser.urlbar.suggest.engines
+//browser.urlbar.suggest.history
+//browser.urlbar.suggest.bookmark
+//browser.urlbar.suggest.mdn
+//browser.urlbar.suggest.importantDates
+//browser.urlbar.suggest.openpage
+//browser.urlbar.suggest.quickactions
+//
+//browser.bookmarks.restore_default_bookmarks
+//app.normandy.first_run
+
+user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.download.dir", "/home/maiz/downloads");
+user_pref("extensions.getAddons.showPane", false);
+user_pref("browser.tabs.insertAfterCurrent", true);
+user_pref("browser.tabs.selectOwnerOnClose", false);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("browser.search.region", "US");
