3238dbb1e8fb9088f5e10d31528ce1f0b21a6601
zenhog <61523958+zenhog@users.noreply.github.com>
2026-07-17 01:23:18 +0100

    2026-07-17 at 01:23:18 on epicenter

---
+++ system/files/usr/local/bin/fzf-wrapper1.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# This wrapper script is invoked by xdg-desktop-portal-termfilechooser.
+#
+# For more information about input/output arguments read `xdg-desktop-portal-termfilechooser(5)`
+
+set -ex
+
+alacritty -e fzf
